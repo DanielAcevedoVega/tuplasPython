cd07e1a25d643996b104bebb3771af2cfc6e6fa8
DanielAcevedoVega <antonydany236@gmail.com>
2024-02-26 09:30:00 -0500

    ejercicio con tuplas

---
+++ main.py
@@ -0,0 +1,14 @@
+# tuplas Informacion, direccion, cursos, experencia laboral
+
+print("""###Almacenamiento de datos###
+Ingrese su informacion, nombre, edad, altura""");
+
+nombre = input("Ingrese el nombre: ");
+edad = input("Ingrese la edad: ");
+altura = input("Ingrese la altura: ");
+informacion = tuple((nombre, edad, altura));
+
+direccion = input("Ingrese la direccion de su residencia: ");
+varDir = tuple((direccion))
+
+cursos = input("Ingrese tus crusos: ");
